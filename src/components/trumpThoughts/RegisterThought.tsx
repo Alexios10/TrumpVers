6753953ba61c7cde3ea306d63c15fe0b8918b9a9
nhuquynhvu <nhu_92@hotmail.com>
2024-11-28 10:59:27 +0100

    new

--- src/components/trumpThoughts/RegisterThought.tsx
+++ src/components/trumpThoughts/RegisterThought.tsx
@@ -63,98 +63,90 @@ const RegisterThought = () => {
 
   return (
     <section className="flex">
-<<<<<<< HEAD
-      <header></header>
-
       <div
         className=" bg-white h-screen flex flex-col"
         style={{ flex: "1 1 40%" }}
       >
         <div className="flex flex-col items-center">
           <h3 className="m-4 text-3xl"> Register new thought</h3>
-=======
-      <div className="flex-1 bg-white h-screen flex flex-col">
-        <div className="flex-1/2 flex flex-col items-center">
-          <header>
-            <h3 className="m-4 text-3xl">Register new thought</h3>
-          </header>
->>>>>>> 5f1d1cfd0a34ab3bd182ac246ab7c3bf22e92b88
-          <div className="w-96 items-start">
-            <div className="mb-2 flex flex-col">
-              <label className="w-24 mr-2 text-xs">Name</label>
-              <input
-                className="w-full bg-gray-200"
-                type="text"
-                name="name"
-                value={name}
-                onChange={handleChange}
-              />
+          <div className="flex-1 bg-white h-screen flex flex-col">
+            <div className="flex-1/2 flex flex-col items-center">
+              <header>
+                <h3 className="m-4 text-3xl">Register new thought</h3>
+              </header>
+              <div className="w-96 items-start">
+                <div className="mb-2 flex flex-col">
+                  <label className="w-24 mr-2 text-xs">Name</label>
+                  <input
+                    className="w-full bg-gray-200"
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                {/* legge til dropdown senere (Options) */}
+                <div className="mb-2 flex flex-col">
+                  <label className="w-24 mr-2 text-xs">Category</label>
+
+                  <input
+                    className="w-full bg-gray-200"
+                    type="text"
+                    name="category"
+                    value={category}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                {/* legge til text area */}
+                <div className="mb-2 flex flex-col">
+                  <label className="w-24 mr-2 text-xs">Thought</label>
+                  <input
+                    className="w-full bg-gray-200"
+                    type="text"
+                    name="thought"
+                    value={thought}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+
+              <button
+                onClick={registerThought}
+                className="m-4 bg-blue-400 text-white p-2 rounded hover:bg-blue-500 shadow-lg"
+              >
+                Register Thought
+              </button>
             </div>
-
-            {/* legge til dropdown senere (Options) */}
-            <div className="mb-2 flex flex-col">
-              <label className="w-24 mr-2 text-xs">Category</label>
-
-              <input
+            <hr className="w-3/4 h-0.5 mx-auto rounded m-4 bg-slate-400" />
+          </div>
+          <div>
+            <div>plassen som skal inneholde thought</div>
+            {/* Dropdown to filter thoughts */}
+            <div className="w-96 mx-auto">
+              <label className="w-24 mr-2 text-xs">Filter by Category</label>
+              <select
                 className="w-full bg-gray-200"
-                type="text"
-                name="category"
-                value={category}
+                name="filterCategory"
+                value={filterCategory}
                 onChange={handleChange}
-              />
+              >
+                {categories.map((category) => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
+              </select>
             </div>
-
-            {/* legge til text area */}
-            <div className="mb-2 flex flex-col">
-              <label className="w-24 mr-2 text-xs">Thought</label>
-              <input
-                className="w-full bg-gray-200"
-                type="text"
-                name="thought"
-                value={thought}
-                onChange={handleChange}
-              />
+            {/* Filtered thoughts list */}
+            <div>
+              <ThoughtList thoughts={filteredThoughts} />
             </div>
           </div>
-
-          <button
-            onClick={registerThought}
-            className="m-4 bg-blue-400 text-white p-2 rounded hover:bg-blue-500 shadow-lg"
-          >
-            Register Thought
-          </button>
         </div>
-        <hr className="w-3/4 h-0.5 mx-auto rounded m-4 bg-slate-400" />
-
-<<<<<<< HEAD
-        <div>
-          <div>plassen som skal inneholde thought</div>
-=======
-        {/* Dropdown to filter thoughts */}
-        <div className="w-96 mx-auto">
-          <label className="w-24 mr-2 text-xs">Filter by Category</label>
-          <select
-            className="w-full bg-gray-200"
-            name="filterCategory"
-            value={filterCategory}
-            onChange={handleChange}
-          >
-            {categories.map((category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        {/* Filtered thoughts list */}
-        <div>
-          <ThoughtList thoughts={filteredThoughts} />
->>>>>>> 5f1d1cfd0a34ab3bd182ac246ab7c3bf22e92b88
-        </div>
+        <div className="flex-1 bg-green-100 h-screen"></div>
       </div>
-
-      <div className="flex-1 bg-green-100 h-screen"></div>
     </section>
   );
 };
