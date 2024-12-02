66a568414057a3361ab4ae1f23a0dcfc3d74b935
Nadeem Ahmed <nadahmed03@hotmail.com>
2024-12-02 14:47:21 +0100

    endret farger i shop

--- src/components/trumpMerch/GetMerchandises.tsx
+++ src/components/trumpMerch/GetMerchandises.tsx
@@ -4,16 +4,16 @@ const GetAllMerchandises = () => {
   return (
     <section className="flex">
       
-      <div className="w-96 p-12 text-lg border-r-2 border-slate-400">
+      <div className="w-96 p-12 text-lg border-r-2 border-slate-400 text-blue-950 ">
         <h2 className="text-2xl font-bold">Trump Merchandise</h2>
         <hr className="w-full h-0.5 mx-auto rounded m-2 bg-slate-400" />
 
-        <ul className="list-group space-y-4 font-semibold mt-8">
-          <li>All</li>
-          <li>T-shirts</li>
-          <li>Hoodies</li>
-          <li>Hats</li>
-          <li>Accessories</li>
+        <ul className="list-group space-y-4 font-semibold mt-8 cursor-pointer">
+          <li className=" hover:text-red-600">All</li>
+          <li className=" hover:text-red-600">T-shirts</li>
+          <li className=" hover:text-red-600">Hoodies</li>
+          <li className=" hover:text-red-600">Hats</li>
+          <li className=" hover:text-red-600">Accessories</li>
         </ul>
       </div>
 
