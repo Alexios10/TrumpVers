880d5bf09cbf6beb1260ab1d0646672ea7a5010a
Sattar <sattar_saud@hotmail.com>
2024-12-02 14:10:44 +0100

    Update to thought page

--- src/components/trumpThoughts/RegisterThought.tsx
+++ src/components/trumpThoughts/RegisterThought.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, useContext, useState } from "react";
 import { ThoughtsContext } from "../../contexts/ThoughtsContext";
 import IThoughtsContext from "../../interfaces/thoughts/IThoughtsContext";
 import ThoughtList from "./ThoughtList";
+import UpdateDeleteThoughts from "./UpdateDeleteThoughts";
 
 const RegisterThought = () => {
   const { postThought, thoughts } = useContext(
@@ -12,6 +13,9 @@ const RegisterThought = () => {
   const [thought, setThought] = useState<string>("");
   const [category, setCategory] = useState<string>(""); // For registering a new thought
   const [filterCategory, setFilterCategory] = useState<string>("All"); // For filtering displayed thoughts
+  const [activePage, setActivePage] = useState<"register" | "admin">(
+    "register"
+  ); // Single state for page control
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
@@ -78,96 +82,134 @@ const RegisterThought = () => {
       ? thoughts
       : thoughts.filter((thought) => thought.category === filterCategory);
 
+  // Switch active page
+  const switchPage = (page: "register" | "admin") => {
+    setActivePage(page);
+  };
+
   return (
     <section className="flex">
       <div
         className=" bg-white h-screen flex flex-col border-r-2 border-slate-400"
         style={{ flex: "1 1 40%" }}
       >
-        <div className="flex flex-col items-center">
-          <h3 className="text-3xl mb-2 text-blue-950"> Register new thought</h3>
-          <div className="flex-1 bg-white h-screen flex flex-col">
-            <div className="flex-1/2 flex flex-col items-center">
-              <div className="w-96 items-start">
-                <div className="mb-2 flex flex-col">
-                  <label className="w-24 mr-2 text-[0.625rem]">Name</label>
-                  <input
-                    className="w-full text-zinc-700 bg-gray-200"
-                    type="text"
-                    name="name"
-                    value={name}
-                    onChange={handleChange}
-                  />
-                </div>
+        <div className="flex gap-4 p-4">
+          <button
+            onClick={() => switchPage("register")}
+            className={`p-2 bg-blue-900 text-white rounded hover:bg-blue-500 shadow-lg text-sm ${
+              activePage === "register" ? "bg-blue-500" : ""
+            }`}
+          >
+            Register New Thought
+          </button>
 
-                <div className="mb-2 flex flex-col">
-                  <label className="w-24 mr-2 text-[0.625rem]">
-                    Select Catagory
-                  </label>
+          <button
+            onClick={() => switchPage("admin")}
+            className={`p-2 bg-blue-900 text-white rounded hover:bg-blue-500 shadow-lg text-sm ${
+              activePage === "admin" ? "bg-blue-500" : ""
+            }`}
+          >
+            Thought Admin
+          </button>
+        </div>
 
-                  <select
-                    className="w-full text-zinc-700 bg-gray-200 "
-                    name="category"
-                    value={category}
-                    onChange={handleChange}
+        {activePage === "admin" && <UpdateDeleteThoughts />}
+
+        {activePage === "register" && (
+          <div
+            className=" bg-white h-screen flex flex-col border-r-2 border-slate-400"
+            style={{ flex: "1 1 40%" }}
+          >
+            <div className="flex flex-col items-center">
+              <h3 className="text-3xl mb-2 text-blue-950">
+                Register new thought
+              </h3>
+              <div className="flex-1 bg-white h-screen flex flex-col">
+                <div className="flex-1/2 flex flex-col items-center">
+                  <div className="w-96 items-start">
+                    <div className="mb-2 flex flex-col">
+                      <label className="w-24 mr-2 text-[0.625rem]">Name</label>
+                      <input
+                        className="w-full text-zinc-700 bg-gray-200"
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={handleChange}
+                      />
+                    </div>
+
+                    <div className="mb-2 flex flex-col">
+                      <label className="w-24 mr-2 text-[0.625rem]">
+                        Select Catagory
+                      </label>
+
+                      <select
+                        className="w-full text-zinc-700 bg-gray-200 "
+                        name="category"
+                        value={category}
+                        onChange={handleChange}
+                      >
+                        {choosenCategoriy.map((category) => (
+                          <option key={category} value={category}>
+                            {category}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+
+                    <div className="mb-2 flex flex-col">
+                      <label className="w-24 mr-2 text-[0.625rem]">
+                        Thought
+                      </label>
+                      <textarea
+                        className="w-full h-24  text-zinc-700 bg-gray-200 "
+                        name="thought"
+                        value={thought}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  </div>
+
+                  <button
+                    onClick={registerThought}
+                    className="mb-2 bg-blue-900 text-white p-2 rounded-sm hover:bg-blue-500 shadow-lg text-xs"
                   >
-                    {choosenCategoriy.map((category) => (
-                      <option key={category} value={category}>
-                        {category}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-
-                <div className="mb-2 flex flex-col">
-                  <label className="w-24 mr-2 text-[0.625rem]">Thought</label>
-                  <textarea
-                    className="w-full h-24  text-zinc-700 bg-gray-200 "
-                    name="thought"
-                    value={thought}
-                    onChange={handleChange}
-                  />
+                    PUBLISH
+                  </button>
                 </div>
               </div>
 
-              <button
-                onClick={registerThought}
-                className="mb-2 bg-blue-900 text-white p-2 rounded-sm hover:bg-blue-500 shadow-lg text-xs"
-              >
-                PUBLISH
-              </button>
+              <hr className="w-4/5 h-0.5 mx-auto rounded m-2 bg-slate-400" />
+              <div className="w-96 mx-auto">
+                <label className="flex flex-col w-32 mr-2 text-xs">
+                  Filter by Category
+                </label>
+                <select
+                  className="w-1/4 bg-gray-200 text-xs"
+                  name="filterCategory"
+                  value={filterCategory}
+                  onChange={handleChange}
+                >
+                  {categories.map((category) => (
+                    <option key={category} value={category}>
+                      {category}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <hr className=" w-2/3 h-0.5 mx-auto rounded m-2 bg-slate-100" />
+              <div className="h-96 overflow-x-hidden overflow-y-auto">
+                <div>
+                  <ThoughtList thoughts={filteredThoughts} />
+                </div>
+              </div>
             </div>
           </div>
-
-          <hr className="w-4/5 h-0.5 mx-auto rounded m-2 bg-slate-400" />
-          <div className="w-96 mx-auto">
-            <label className="flex flex-col w-32 mr-2 text-xs">
-              Filter by Category
-            </label>
-            <select
-              className="w-1/4 bg-gray-200 text-xs"
-              name="filterCategory"
-              value={filterCategory}
-              onChange={handleChange}
-            >
-              {categories.map((category) => (
-                <option key={category} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
-          </div>
-          <hr className=" w-2/3 h-0.5 mx-auto rounded m-2 bg-slate-100" />
-          <div className="h-96 overflow-x-hidden overflow-y-auto">
-            <div>
-              <ThoughtList thoughts={filteredThoughts} />
-            </div>
-          </div>
-        </div>
+        )}
       </div>
 
       <div
-        className=" m-8 flex-1 bg-green-100 h-screen"
+        className="m-8 flex-1 bg-green-100 h-screen"
         style={{ flex: "1 1 60%" }}
       ></div>
     </section>
