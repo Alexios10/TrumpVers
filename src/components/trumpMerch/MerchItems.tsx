dbabd78206d784f9f66a18df3beba53719b3e77e
nhuquynhvu <nhu_92@hotmail.com>
2024-12-02 12:06:55 +0100

    merch style

--- src/components/trumpMerch/MerchItems.tsx
+++ src/components/trumpMerch/MerchItems.tsx
@@ -11,7 +11,7 @@ const MerchItem: FC<IMerch> = ({
 }) => {
   return (
     <article className="flex justify-center my-4">
-      <div className="w-80 h-96 cursor-pointer flex flex-col rounded-sm p-1 shadow-lg border-solid border-2 border-blue-950 border-opacity-20 hover:border-4">
+      <div className="w-80 h-auto cursor-pointer flex flex-col rounded-sm p-1 shadow-lg border-solid border-2 border-blue-950 border-opacity-20 hover:border-4">
         <div className="flex-1 m-8">
           <img
             src={MerchService.getImageEndpoint() + image}
@@ -19,18 +19,21 @@ const MerchItem: FC<IMerch> = ({
             className="object-contain w-full h-full"
           />
         </div>
-
-        <div className="h-40 bg-white p-4 flex flex-col justify-between">
+        <hr />
+        <div className="p-2 bg-white flex flex-col justify-between">
           <h3 className="text-xl font-semibold text-gray-800">{name}</h3>
-          <p className="text-sm text-gray-600 mt-2 line-clamp-3">
-            {description}
-          </p>
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between">
+            <p className="text-sm text-gray-600  line-clamp-3">{description}</p>
             <p className="text-lg font-medium text-gray-900">{price} $</p>
-            <p className="text-sm text-gray-500">{quantity} left</p>
           </div>
         </div>
-        <button>Add to Cart</button>
+        <button className="bg-blue-700 text-white font-semibold py-2 px-3 rounded-md hover:bg-blue-600 transition-colors duration-300">
+          Add to Cart
+        </button>
+        <div className="flex justify-between items-center text-xs">
+          <p className="text-green-600">In stock</p>
+          <p className="mt-2  text-gray-500 text-end">{quantity} left</p>
+        </div>
       </div>
     </article>
   );
