737ccc0ec168984c6c4f02cd159517cfe6a0adc9
Nadeem Ahmed <nadahmed03@hotmail.com>
2024-12-04 14:50:35 +0100

    design på staff

---
+++ src/components/staffMembers/Staff.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import StaffMemberItems from "./StaffMemberItems";
+import { StaffMemberContext } from "../../contexts/StaffMembersContext";
+import IStaffContext from "../../interfaces/staffMembers/IStaffContext";
+
+const Staff = () => {
+  const { members } = useContext(StaffMemberContext) as IStaffContext;
+
+  const createAndGetStaffJSX = () => {
+    const staffMemberJSX = members.map((member) => {
+      return (
+        <StaffMemberItems
+          key={member.id}
+          name={member.name}
+          image={member.image}
+          description={member.description}
+          title={member.title}
+          email={member.email}
+        />
+      );
+    });
+
+    return staffMemberJSX;
+  };
+
+  return <div>{createAndGetStaffJSX()}</div>;
+};
+
+export default Staff;
--- src/routing/AppRouting.tsx
+++ src/routing/AppRouting.tsx
@@ -3,7 +3,7 @@ import MainNavigation from "../components/shared/MainNavigation";
 import HomePage from "../pages/HomePage";
 import GetAllMerchandises from "../components/trumpMerch/GetMerchandises";
 import RegisterMember from "../components/staffMembers/RegisterMember";
-import StaffmemberList from "../components/staffMembers/StaffMemberList";
+import Staff from "../components/staffMembers/Staff";
 
 const AppRouting = () => {
   return (
@@ -13,7 +13,7 @@ const AppRouting = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<GetAllMerchandises />}></Route>
         <Route path="/registerMembers" element={<RegisterMember />}></Route>
-        <Route path="/staff" element={<StaffmemberList />}></Route>
+        <Route path="/staff" element={<Staff />}></Route>
       </Routes>
     </BrowserRouter>
   );
