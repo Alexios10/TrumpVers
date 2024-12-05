cf858db3c7099a023dc12d2dce8af14e1941652b
Sattar <sattar_saud@hotmail.com>
2024-12-05 15:03:44 +0100

    svg removed

--- src/components/staffMembers/StaffMemberItems.tsx
+++ src/components/staffMembers/StaffMemberItems.tsx
@@ -11,29 +11,31 @@ const StaffMemberItems: FC<IStaff> = ({
   email,
 }) => {
   return (
-    <article className="flex justify-center p-4 ">
-      <div className="flex flex-col  w-80 h-auto">
-        <div className="flex-1 justify-center ">
-          <img
-            src={StaffMembersService.getImageEndpoint() + image}
-            alt={name}
-            className="object-contain w-full h-40"
-          />
-        </div>
+    <section>
+      {" "}
+      <article className="flex justify-center p-4 ">
+        <div className="flex flex-col  w-80 h-auto">
+          <div className="flex-1 justify-center ">
+            <img
+              src={StaffMembersService.getImageEndpoint() + image}
+              alt={name}
+              className="object-contain w-full h-40"
+            />
+          </div>
+          <div className="flex flex-col justify-center text-center rounded-sm bg-blue-950 bg-opacity-30 text-white font-bold m-2 h-36 shadow-lg">
+            <h3 className="text-xl">{name}</h3>
+            <p>{title}</p>
+            <p>{description}</p>
 
-        <div className="flex flex-col justify-center text-center rounded-sm bg-blue-950 bg-opacity-30 text-white font-bold m-2 h-36 shadow-lg">
-          <h3 className="text-xl">{name}</h3>
-          <p>{title}</p>
-          <p>{description}</p>
-
-          <p className="flex items-center justify-center text-sm">
-            <LuMail className="mr-2" />D
-          </p>
-          <p className="flex justify-center text-sm">{email}</p>
-          <p className="flex justify-center text-sm">{email}</p>
+            <p className="flex items-center justify-center text-sm">
+              <LuMail className="mr-2" />D
+            </p>
+            <p className="flex justify-center text-sm">{email}</p>
+            <p className="flex justify-center text-sm">{email}</p>
+          </div>
         </div>
-      </div>
-    </article>
+      </article>
+    </section>
   );
 };
 
