739242bd48f8cd3cf96f41e91a6233be906b1021
Sattar <sattar_saud@hotmail.com>
2024-12-05 15:06:29 +0100

    svg removed

--- src/components/staffMembers/StaffMemberItems.tsx
+++ src/components/staffMembers/StaffMemberItems.tsx
@@ -42,24 +42,7 @@ const StaffMemberItems: FC<IStaff> = ({
             <p className="flex items-center justify-center text-sm">
               <LuMail className="mr-2" />
             </p>
-            <p className="flex justify-center text-sm">
-              <svg
-                className="mr-2"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" />{" "}
-                <rect x="3" y="5" width="18" height="14" rx="2" />{" "}
-                <polyline points="3 7 12 13 21 7" />
-              </svg>{" "}
-              {email}
-            </p>
+            <p className="flex justify-center text-sm">{email}</p>
             <p className="flex justify-center text-sm">{email}</p>
           </div>
         </div>
