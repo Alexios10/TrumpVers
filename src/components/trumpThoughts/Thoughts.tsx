2e8b980275f5967c0b80b16cb0b6424d4194bf27
nhuquynhvu <nhu_92@hotmail.com>
2024-12-09 12:17:47 +0100

    thought side og thought admin

--- src/components/trumpThoughts/Thoughts.tsx
+++ src/components/trumpThoughts/Thoughts.tsx
@@ -2,7 +2,13 @@ import ThoughtList from "./ThoughtList";
 
 const Thoughts = () => {
   return (
-    <div>
+    <div className="mx-60">
+      <h2 className="text-center text-bold text-6xl text-blue-950 mb-2">
+        From the President's Desk:
+      </h2>
+      <h3 className="text-center text-bold text-5xl text-blue-950 mb-8">
+        Thoughtful Dialogues on Our Country's Future
+      </h3>
       <ThoughtList />
     </div>
   );
