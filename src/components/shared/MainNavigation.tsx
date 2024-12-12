6666b44b46c4cf081999756c17e37dc55edb008d
Sattar <sattar_saud@hotmail.com>
2024-12-13 00:42:53 +0100

    main navigation optimazition

--- src/components/shared/MainNavigation.tsx
+++ src/components/shared/MainNavigation.tsx
@@ -4,29 +4,55 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { IoIosClose } from "react-icons/io";
 
 const MainNavigation = () => {
-  // Lokal tilstand for å spore om menyen er åpen eller lukket
+  // Lokal tilstand for å spore om mobil menyen er åpen eller lukket
   const [isOpen, setIsOpen] = useState(false);
 
   // Henter informasjon om gjeldende rute
   const location = useLocation();
 
-  // Funksjon for å toggle menyens åpen/lukket tilstand
+  // Funksjon for åpen/lukket meny
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  // Funksjon for å sjekke om en rute er aktiv, returnerer en CSS-klasse for å markere aktiv tilstand
-  const isActive = (path) => {
+  // Funksjon for beholde fargen på den nåværende side
+  const isActive = (path: string) => {
     return location.pathname === path ? "text-red-600" : "text-sky-950";
   };
+
+  const links = [
+    {
+      path: "/thoughts",
+      name: "THOUGHTS",
+    },
+    {
+      path: "/shop",
+      name: "MERCH",
+    },
+    {
+      path: "/staff",
+      name: "STAFF",
+    },
+    {
+      path: "/thoughtsAdmin",
+      name: "THOUGHTS ADMIN",
+    },
+    {
+      path: "/merchAdmin",
+      name: "MERCH ADMIN",
+    },
+    {
+      path: "/registerMembers",
+      name: "STAFF ADMIN",
+    },
+  ];
 
   return (
     <>
       <nav className="bg-white mx-8 mb-6">
-        {/* Logo med en lenke tilbake til hovedsiden */}
-
         <div className=" flex items-center justify-between mb-4 ">
-          <div>
+          {/* Logo med en lenke tilbake til hovedsiden */}
+          <div className="w-full">
             <Link to="/">
               <img
                 className="w-44"
@@ -35,144 +61,71 @@ const MainNavigation = () => {
               />
             </Link>
           </div>
-          {/* Navigation Meny: Flex-meny for navigasjon */}
-          <ul className="hidden md:flex md:flex-row md:space-x-8 text-xl">
-            {/* Liste over navigasjonslenker med dynamisk klasse basert på aktiv rute */}
-            <li>
-              <Link
-                to="/thoughts"
-                className={`nav-links ${isActive("/thoughts")}`}
+          {/* Nav Links */}
+          {links.map((link, index) => {
+            return (
+              <ul
+                key={index}
+                className="hidden w-full md:flex md:flex-row md:space-x-8 text-xl"
               >
-                THOUGHTS
-              </Link>
-            </li>
-            <li>
-              <Link to="/shop" className={`nav-links ${isActive("/shop")}`}>
-                MERCH
-              </Link>
-            </li>
-            <li>
-              <Link to="/staff" className={`nav-links ${isActive("/staff")}`}>
-                STAFF
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/thoughtsAdmin"
-                className={`nav-links ${isActive("/thoughtsAdmin")}`}
-              >
-                THOUGHTS ADMIN
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/merchAdmin"
-                className={`nav-links ${isActive("/merchAdmin")}`}
-              >
-                MERCH ADMIN
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/registerMembers"
-                className={`nav-links ${isActive("/registerMembers")}`}
-              >
-                STAFF ADMIN
-              </Link>
-            </li>
-          </ul>
+                <li className={`nav-links ${isActive(`${link.path}`)}`}>
+                  <Link to={link.path}>{link.name}</Link>
+                </li>
+              </ul>
+            );
+          })}
         </div>
         {/* Horisontal linje som skiller navigasjonsbaren fra annet innhold */}
         <hr className="border-red-600" />
       </nav>
 
-      {/* Mobil Nav */}
-      <div
-        className={`${
-          isOpen &&
-          "w-screen h-screen  top-0 left-0 bg-black/30 fixed md:hidden"
-        }`}
-      ></div>
-      <div className="md:hidden flex flex-col absolute right-0 top-0">
-        {/* Viser enten et lukk-ikon eller hamburger-ikon basert på isOpen-tilstanden */}
-        {isOpen ? (
-          <div className="flex flex-col bg-black/60 p-5">
-            <div className="flex justify-end">
-              <button
-                onClick={toggleMenu}
-                className="flex items-center justify-center border-2 mb-2 w-10 h-10 bg-white rounded-md shadow-md transition duration-300 ease-in-out transform hover:scale-105"
-              >
-                <IoIosClose />
-              </button>
-            </div>
+      {/* Mobile Nav Overlay */}
+      {isOpen && (
+        <div
+          className="w-screen h-screen top-0 left-0 bg-black/30 fixed md:hidden"
+          onClick={toggleMenu}
+        ></div>
+      )}
 
-            {/* nav links */}
-            <ul className="flex flex-col text-xl space-y-10">
-              <li className="">
-                <Link
-                  to="/thoughts"
-                  className={`nav-links text-white ${isActive("/thoughts")}`}
-                >
-                  THOUGHTS
-                </Link>
-              </li>
+      {/* Hamburger Ikonet */}
+      <div className="md:hidden p-5 fixed top-0 right-0">
+        {!isOpen && (
+          <button
+            onClick={toggleMenu}
+            className="flex items-center justify-center border-2 w-10 h-10 bg-white rounded-md shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+          >
+            <RxHamburgerMenu />
+          </button>
+        )}
+      </div>
+
+      {/* Mobil Nav Meny */}
+      <div
+        className={`md:hidden flex flex-col fixed top-0 right-0 h-screen bg-black/60 p-5 transform ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        } transition-transform duration-300 ease-in-out`}
+      >
+        <div className="flex justify-end">
+          {/* Lukk Knappen */}
+          <button
+            onClick={toggleMenu}
+            className="flex items-center justify-center border-2 mb-2 w-10 h-10 bg-white rounded-md shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+          >
+            <IoIosClose />
+          </button>
+        </div>
+        {/* Nav links */}
+        {links.map((link, index) => {
+          return (
+            <ul key={index} className="flex flex-col text-xl space-y-10 mt-5">
               <li>
-                <Link
-                  to="/shop"
-                  className={`nav-links text-white ${isActive("/shop")}`}
-                >
-                  MERCH
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/staff"
-                  className={`nav-links text-white ${isActive("/staff")}`}
-                >
-                  STAFF
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/thoughtsAdmin"
-                  className={`nav-links text-white ${isActive(
-                    "/thoughtsAdmin"
-                  )}`}
-                >
-                  THOUGHTS ADMIN
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/merchAdmin"
-                  className={`nav-links text-white ${isActive("/merchAdmin")}`}
-                >
-                  MERCH ADMIN
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/registerMembers"
-                  className={`nav-links text-white ${isActive(
-                    "/registerMembers"
-                  )}`}
-                >
-                  STAFF ADMIN
+                <Link to={link.path} className="nav-links text-white">
+                  {link.name}
                 </Link>
               </li>
             </ul>
-          </div>
-        ) : (
-          <div className="p-5">
-            {" "}
-            <button
-              onClick={toggleMenu}
-              className="flex items-center justify-center border-2 w-10 h-10 bg-white rounded-md shadow-md transition duration-300 ease-in-out transform hover:scale-105"
-            >
-              <RxHamburgerMenu />
-            </button>
-          </div>
-        )}
+          );
+        })}
       </div>
     </>
   );
